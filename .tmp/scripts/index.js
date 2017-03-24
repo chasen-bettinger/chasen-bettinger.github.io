bd4a7fb6c29637852dc0da408d87f976c20d5405
chasen-bettinger <bettingerchasen@gmail.com>
2017-03-24 13:31:39 -0400

    fix images

---
+++ .tmp/scripts/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// Change picture based on hover
+$(function () {
+    $("img").hover(function () {
+        var src = $(this).attr("src").match(/[^\.]+/) + "hover.png";
+        $(this).css('min-height', $(this).height());
+        $(this).attr("src", src);
+    }, function () {
+        var src = $(this).attr("src").replace("hover.png", ".png");
+        $(this).attr("src", src);
+    });
+});
+//# sourceMappingURL=index.js.map
